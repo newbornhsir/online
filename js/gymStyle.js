702ecd63f08655438536d49f2854ad9695914261
newbornhsir <765302230@qq.com>
2016-08-24 20:37:46 +0800

    gym

---
+++ js/gymStyle.js
@@ -0,0 +1,10 @@
+var imgList = document.getElementsByClassName("coache")[0].children;
+var coache = document.getElementsByClassName("coache")[0];
+//var w = document.getElementsByClassName("coache")[0].offsetWidth;
+//console.log(w);
+//var h = coache.getAttribute("class");
+//console.log(h);
+for(var i = 0 ; i < imgList.length ; i++){
+	imgList[i].index = i+1;
+	console.log(imgList[i].offsetWidth);
+}
